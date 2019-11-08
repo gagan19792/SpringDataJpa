f73716edb44649915bc4ff05b0f9f9720d9fb992
gagan19792 <gagan.ubbey@gmail.com>
2019-11-07 19:39:39 -0800

    updated springboot version to 2.1.7

--- src/test/java/com/learn/spring/data/jpa/SpringDataJpaApplicationTests.java
+++ src/test/java/com/learn/spring/data/jpa/SpringDataJpaApplicationTests.java
@@ -1,10 +1,10 @@
 package com.learn.spring.data.jpa;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringDataJpaApplicationTests {
+class SpringDataJOpaApplicationTests {
 
 	@Test
 	void contextLoads() {
